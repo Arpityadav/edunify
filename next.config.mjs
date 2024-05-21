7df3f24953e7bb9f24f266d5647600ecfa855647
Arpit <arpit.yadav1912@gmail.com>
2024-05-21 19:46:18 +0530

    Added port in next.config

--- next.config.mjs
+++ next.config.mjs
@@ -15,6 +15,7 @@ const nextConfig = {
     env: {
         MYSQL_HOST: process.env.MYSQL_HOST && process.env.MYSQL_HOST.replace(/,$/g, ''),
         MYSQL_USER: process.env.MYSQL_USER && process.env.MYSQL_USER.replace(/,$/g, ''),
+        MYSQL_PORT: process.env.MYSQL_PORT && process.env.MYSQL_PORT.replace(/,$/g, ''),
         MYSQL_PASSWORD: process.env.MYSQL_PASSWORD && process.env.MYSQL_PASSWORD.replace(/,$/g, ''),
         MYSQL_DATABASE: process.env.MYSQL_DATABASE && process.env.MYSQL_DATABASE.replace(/,$/g, ''),
         URL: process.env.URL && process.env.URL.replace(/,$/g, ''),
